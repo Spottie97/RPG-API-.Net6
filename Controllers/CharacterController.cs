6b0dc58b9f555f8f4542cf3991f48bc05755db30
Reinhardt Erasmus <reinhardterasmus@gmail.com>
2022-11-30 15:05:04 +0200

    ICharacterService & CharacterService

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using dotnet_rpg.Services.CharacterService;
 using Microsoft.AspNetCore.Mvc;
 
 namespace dotnet_rpg.Controllers
@@ -10,28 +11,29 @@ namespace dotnet_rpg.Controllers
     [Route("api/[controller]")]
     public class CharacterController : ControllerBase
     {
-        private static List<Character> characters = new List<Character> {
-            new Character(),
-            new Character{ Id = 1, Name = "Sam" }
-        };
-        
+        private readonly ICharacterService characterService;
+        public CharacterController(ICharacterService characterService)
+        {
+            this.characterService = characterService;
+        }
+
         [HttpGet("GetALL")]
         public ActionResult<List<Character>> Get()
         {
-            return Ok(characters);
+            return Ok(characterService.GetAllCharacters());
         }
 
         [HttpGet("{id}")]
         public ActionResult<Character>GetSingle(int id)
         {
-            return Ok(characters.FirstOrDefault(c => c.Id == id));
+            return Ok(characterService.GetCharacterById(id));
         }
 
         [HttpPost]
-        public ActionResult<List<Character>> AddCharacter(Character newCharacter)
+        public ActionResult<List<Character>>AddCharacter(Character newCharacter)
         {
-            characters.Add(newCharacter);
-            return Ok(characters);
+
+            return Ok(characterService.AddCharacter(newCharacter));
         }
     }
 }
